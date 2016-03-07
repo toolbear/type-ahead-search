06c2874ebe65f7fbdeed0a5654d97d343e515d20
Tim Taylor <tim@tool-man.org>
2016-03-07 15:40:11 -0800

    a word iterator

--- src/test/java/WordIteratorTest.java
+++ src/test/java/WordIteratorTest.java
@@ -10,33 +10,71 @@ public class WordIteratorTest {
   @Rule public MockitoRule mockito = MockitoJUnit.rule();
 
   @Test
+  public void hasNext() {
+    assertThat(new WordIterator("").hasNext()).isFalse();
+    Iterator<CharSequence> subject = new WordIterator("word");
+    assertThat(subject.hasNext()).isTrue();
+    subject.next();
+    assertThat(subject.hasNext()).isFalse();
+  }
+
+  @Test
+  public void next() {
+    assertThat(new WordIterator("").next()).isNull();
+    Iterator<CharSequence> subject = new WordIterator("word");
+    assertThat(subject.next()).isNotNull();
+    assertThat(subject.next()).isNull();
+  }
+
+  @Test
   public void empty() {
-    Iterator<CharSequence> subject = new WordIterator("");
-    assertThat(subject.hasNext()).isFalse();
-    assertThat(subject.next()).isNull();
+    assertThat(new WordIterator("")).isEmpty();
   }
 
   @Test
-  public void allWhitespace() {
-    Iterator<CharSequence> subject = new WordIterator(" \t");
-    assertThat(subject.hasNext()).isFalse();
-    assertThat(subject.next()).isNull();
+  public void justWhitespace() {
+    assertThat(new WordIterator(" \t")).isEmpty();
   }
 
   @Test
-  @Ignore("pending")
-  public void allNonWords() {}
+  public void justNonWords() {
+    assertThat(new WordIterator("# ? $")).isEmpty();
+  }
 
   @Test
   public void oneWord() {
-    Iterator<CharSequence> subject = new WordIterator("booyah");
-    assertThat(subject.hasNext()).isTrue();
-    assertThat(subject.next()).isEqualTo("booyah");
-    assertThat(subject.hasNext()).isFalse();
-    assertThat(subject.next()).isNull();
+    assertThat(new WordIterator("Taken")).contains("Taken");
   }
 
   @Test
-  @Ignore("pending")
-  public void embeddedPunctuation() {}
+  public void multipleWords() {
+    assertThat(new WordIterator("The Princess Bride"))
+      .containsExactly("The", "Princess", "Bride");
+  }
+
+  @Test
+  public void embeddedPunctuation() {
+    assertThat(new WordIterator("Borat: Cultural Learnings of America For Make Benefit Glorious Nation of Kazakhstan"))
+      .startsWith("Borat", "Cultural");
+  }
+
+  @Test
+  public void numeric() {
+    assertThat(new WordIterator("300")).startsWith("300");
+  }
+
+  @Test
+  public void alphanumeric() {
+    assertThat(new WordIterator("se7en")).startsWith("se7en");
+  }
+
+  @Test
+  public void underscores() {
+    assertThat(new WordIterator("snake_case")).startsWith("snake_case");
+  }
+
+  @Test
+  public void hyphens() {
+    assertThat(new WordIterator("Kick-Ass")).startsWith("Kick-Ass");
+  }
 }
