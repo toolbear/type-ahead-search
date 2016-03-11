ceee66a1269acdf324e5cd4e7ce1f5fc0322c0e5
Tim Taylor <tim@tool-man.org>
2016-03-11 01:21:17 -0800

    function for comparing CharSequences

--- src/main/java/tas/Functions.java
+++ src/main/java/tas/Functions.java
@@ -1,20 +1,106 @@
 package tas;
 
 public final class Functions {
+  private static final CharSequence EMPTY_SEQUENCE = "";
+
+  public static CharSequence commonPrefix(CharSequence left, CharSequence right) {
+    int end = 0;
+    for (int n = Math.min(left.length(), right.length()); end < n; end++) {
+      if (left.charAt(end) != right.charAt(end)) break;
+    }
+    return left.subSequence(0, end);
+  }
+
+  public static Comparison compare(CharSequence left, CharSequence right) {
+    return compare(left, left.length(), right, right.length());
+  }
+
+  private static Comparison compare(CharSequence l, int llen, CharSequence r, int rlen) {
+    if (llen == 0 && rlen == 0) {
+      return new Comparison(EMPTY_SEQUENCE, EMPTY_SEQUENCE, EMPTY_SEQUENCE, Comparison.EQUIVALENT);
+    } else if (llen == 0) {
+      return new Comparison(EMPTY_SEQUENCE, r, EMPTY_SEQUENCE, Comparison.LEFT_EMPTY);
+    } else if (rlen == 0) {
+      return new Comparison(l, EMPTY_SEQUENCE, EMPTY_SEQUENCE, Comparison.RIGHT_EMPTY);
+    }
+
+    int end = 0;
+    int cmp = 0;
+    for (int n = Math.min(llen, rlen); end < n; end++) {
+      cmp = Character.compare(l.charAt(end), r.charAt(end));
+      if (cmp != 0) break;
+    }
+    if (llen == end && rlen == end) {
+      return new Comparison(EMPTY_SEQUENCE, EMPTY_SEQUENCE, l, Comparison.EQUIVALENT);
+    } else if (llen == end) {
+      return new Comparison(EMPTY_SEQUENCE, r.subSequence(end, rlen), l, Comparison.LEFT_SUBSEQUENCE);
+    } else if (rlen == end) {
+      return new Comparison(l.subSequence(end, llen), EMPTY_SEQUENCE, r, Comparison.RIGHT_SUBSEQUENCE);
+    } else {
+      Comparison.Relation relation;
+      if (cmp < 0) relation = Comparison.PRECEDES;
+      else if (cmp > 0) relation = Comparison.SUCCEEDS;
+      else throw new IllegalStateException("expected sequences to differ");
+
+      return new Comparison(l.subSequence(end, llen), r.subSequence(end, rlen), l.subSequence(0, end), relation);
+    }
+  }
+
+  public static class Comparison {
+    public static final Relation LEFT_EMPTY = Relation.LEFT_EMPTY;
+    public static final Relation LEFT_SUBSEQUENCE = Relation.LEFT_SUBSEQUENCE;
+    public static final Relation PRECEDES = Relation.PRECEDES;
+    public static final Relation EQUIVALENT = Relation.EQUIVALENT;
+    public static final Relation SUCCEEDS = Relation.SUCCEEDS;
+    public static final Relation RIGHT_SUBSEQUENCE = Relation.RIGHT_SUBSEQUENCE;
+    public static final Relation RIGHT_EMPTY = Relation.RIGHT_EMPTY;
+
+    public final CharSequence leftSuffix;
+    public final CharSequence rightSuffix;
+    public final CharSequence common;
+    public final Relation relation;
+
+    Comparison(CharSequence leftSuffix,
+               CharSequence rightSuffix,
+               CharSequence common,
+               Relation relation) {
+      this.leftSuffix = leftSuffix;
+      this.rightSuffix = rightSuffix;
+      this.common = common;
+      this.relation = relation;
+    }
+
+    @Override
+    public String toString() {
+      return String.format("❮%s⌊%s⌋ %c %s⌊%s⌋❯", common, leftSuffix, relation.symbol, common, rightSuffix);
+    }
+
+    public static enum Relation {
+      LEFT_EMPTY('≪', -1),
+      LEFT_SUBSEQUENCE('⊂', -1),
+      PRECEDES('≺', -1),
+      EQUIVALENT('∼', 0),
+      RIGHT_EMPTY('≫', 1),
+      RIGHT_SUBSEQUENCE('⊃', 1),
+      SUCCEEDS('≻', 1);
+
+      public final char symbol;
+      public final short signum;
+
+      Relation(char symbol, int signum) {
+        this.symbol = symbol;
+        this.signum = (short)signum;
+      }
+    }
+  }
+
   public static class Tuple2<A,B> {
     public final A _1;
     public final B _2;
+
     public Tuple2(A _1, B _2) {
       this._1 = _1;
       this._2 = _2;
     }
-  }
-
-  public static CharSequence commonPrefix(CharSequence a, CharSequence b) {
-    int end = 0;
-    for (int n = Math.min(a.length(), b.length()); end < n; end++) {
-      if (a.charAt(end) != b.charAt(end)) break;
-    }
-    return a.subSequence(0, end);
   }
 }
--- src/test/java/tas/FunctionsTest.java
+++ src/test/java/tas/FunctionsTest.java
@@ -5,41 +5,93 @@ import org.junit.*;
 import org.junit.experimental.runners.Enclosed;
 import org.junit.runner.RunWith;
 import static org.assertj.core.api.Assertions.*;
+import tas.Functions.Comparison;
 
 @RunWith(Enclosed.class)
 public class FunctionsTest {
-
   public static class ComparingCharSequences {
-    private BiFunction<CharSequence,CharSequence,CharSequence> subject = Functions::commonPrefix;
-
-    @Test
-    public void bothEmpty() {
-      assertThat(subject.apply("", "").toString()).isEmpty();
-    }
+    private BiFunction<CharSequence,CharSequence,Comparison> subject = Functions::compare;
 
     @Test
     public void leftEmpty() {
-      assertThat(subject.apply("", "dog").toString()).isEmpty();
+      Comparison c = subject.apply("", "dog");
+      assertThat(c.leftSuffix).isEmpty();
+      assertThat(c.rightSuffix.toString()).isEqualTo("dog");
+      assertThat(c.common).isEmpty();
+      assertThat(c.relation).isEqualTo(Comparison.LEFT_EMPTY);
     }
 
     @Test
     public void rightEmpty() {
-      assertThat(subject.apply("dog", "").toString()).isEmpty();
+      Comparison c = subject.apply("dog", "");
+      assertThat(c.leftSuffix.toString()).isEqualTo("dog");
+      assertThat(c.rightSuffix).isEmpty();
+      assertThat(c.common).isEmpty();
+      assertThat(c.relation).isEqualTo(Comparison.RIGHT_EMPTY);
     }
 
     @Test
     public void identical() {
-      assertThat(subject.apply("dog", "dog").toString()).isEqualTo("dog");
+      Comparison c = subject.apply("dog", "dog");
+      assertThat(c.leftSuffix).isEmpty();
+      assertThat(c.rightSuffix).isEmpty();
+      assertThat(c.common.toString()).isEqualTo("dog");
+      assertThat(c.relation).isEqualTo(Comparison.EQUIVALENT);
+    }
+
+    @Test
+    public void bothEmpty() {
+      Comparison c = subject.apply("", "");
+      assertThat(c.leftSuffix).isEmpty();
+      assertThat(c.rightSuffix).isEmpty();
+      assertThat(c.common).isEmpty();
+      assertThat(c.relation).isEqualTo(Comparison.EQUIVALENT);
     }
 
     @Test
     public void leftSubSequence() {
-      assertThat(subject.apply("do", "dog").toString()).isEqualTo("do");
+      Comparison c = subject.apply("do", "dog");
+      assertThat(c.leftSuffix).isEmpty();
+      assertThat(c.rightSuffix.toString()).isEqualTo("g");
+      assertThat(c.common.toString()).isEqualTo("do");
+      assertThat(c.relation).isEqualTo(Comparison.LEFT_SUBSEQUENCE);
     }
 
     @Test
     public void rightSubSequence() {
-      assertThat(subject.apply("dog", "do").toString()).isEqualTo("do");
+      Comparison c = subject.apply("dog", "do");
+      assertThat(c.leftSuffix.toString()).isEqualTo("g");
+      assertThat(c.rightSuffix).isEmpty();
+      assertThat(c.common.toString()).isEqualTo("do");
+      assertThat(c.relation).isEqualTo(Comparison.RIGHT_SUBSEQUENCE);
+    }
+
+    @Test
+    public void preceding() {
+      Comparison c = subject.apply("aardvark", "about");
+      assertThat(c.leftSuffix.toString()).isEqualTo("ardvark");
+      assertThat(c.rightSuffix.toString()).isEqualTo("bout");
+      assertThat(c.common.toString()).isEqualTo("a");
+      assertThat(c.relation).isEqualTo(Comparison.PRECEDES);
+    }
+
+    @Test
+    public void succeeding() {
+      Comparison c = subject.apply("catbox", "catatonic");
+      assertThat(c.leftSuffix.toString()).isEqualTo("box");
+      assertThat(c.rightSuffix.toString()).isEqualTo("atonic");
+      assertThat(c.common.toString()).isEqualTo("cat");
+      assertThat(c.relation).isEqualTo(Comparison.SUCCEEDS);
+    }
+
+    @Test
+    public void differByFirstCharacter() {
+      assertThat(subject.apply("a", "b").relation).isNotEqualTo(Comparison.EQUIVALENT);
+    }
+
+    @Test
+    public void differByLastCharacter() {
+      assertThat(subject.apply("ab", "aa").relation).isNotEqualTo(Comparison.EQUIVALENT);
     }
   }
 }
